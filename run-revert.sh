a5b9af186172cb8d6dcb11f40ba32ea844c7efda
bro2020 <arm121@i.ua>
2022-01-05 01:28:23 +0200

    Первое подобие рабочих скриптов

--- run-revert.sh
+++ run-revert.sh
@@ -1,4 +1,36 @@
-
+#!/bin/bash
 export LD_LIBRARY_PATH=./lib64:"$LD_LIBRARY_PATH"
 export PATH=./:"$PATH"
-adb devices
+APPS_LIST=`cat ./APPS_LIST`
+DEV=$(adb devices | tail +2)
+
+worker(){
+for APPS in $APPS_LIST
+do
+adb shell pm install -k --user 0 $APPS
+done
+echo 'Процесс восстановления завершен'
+exit 0
+}
+
+input(){
+read -p "Введите название приложения: " APPS_LIST
+worker
+}
+
+selected(){
+read -p "Введите 1 для одиночного восстановления приложения, или enter для восстановления списка: " sel
+case $sel in
+  1) input ;;
+  '') worker ;;
+  *) echo "Неверный ввод!"; selected ;;
+esac
+}
+
+if [[ $DEV = '' ]]; then
+echo 'Телефон не обнаружен'
+exit 1
+else
+echo "Обнаружен телефон $DEV"
+selected
+fi
